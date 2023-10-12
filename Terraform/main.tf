edd983373e89124d2f0529ccc8373e9524391149
“jatinbhalla” <jatinbhalla18@gmail.comgit config --global user.email jatinbhalla18@gmail.com>
2023-10-12 15:06:28 +0530

    path

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,30 +1,30 @@
 
 module "aduser" {
   depends_on = [ module.adgroup ]
-  source = "../gpandmem/admember"
+  source = "../Terraform/admember"
   for_each = try (var.aduser,{})
   user_principal_name1 = "${each.value.aduser}"
   display_name1 = var.adgroup
 }
 
 module "adgroup" {
-  source = "../gpandmem/adgroup"
+  source = "../Terraform/adgroup"
   display_name1 = var.adgroup
 }
 
 module "api" {
-  source = "../gpandmem/adapi"
+  source = "../Terraform/adapi"
   api= var.apiname
 }
 
 # module "apipermission" {
-#   source = "../gpandmem/apipermission"
+#   source = "../Terraform/apipermission"
 #   #api= var.apiname
   
 # }
 
 # module "azurerm_sql_server" {
-#   source = "../gpandmem/ipwhitelisting"
+#   source = "../Terraform/ipwhitelisting"
 #   firewall_rule_name = var.rule_name
 #   end_ip_address = var.end_ip
 #   start_ip_address = var.start_ip
